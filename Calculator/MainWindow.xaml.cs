8b5de3a454e246e171cc9e4ce5adb3382bfc830b
ejrach <eric.rach@gmail.com>
2019-10-15 14:21:14 -0500

    added button click events and code behind number and operation buttons

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,11 +20,92 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        //By default, these are private.
+        double lastNumber, result;
+
+        //Constructor
         public MainWindow()
         {
             InitializeComponent();
 
-            lblResult.Content = "1432";
+            //To create event handlers (shown below), type the name of the button with '.Click', type '+=' then tab.
+            //This also creates the button click handlers.
+            btnAC.Click += BtnAC_Click;
+            btnNegative.Click += BtnNegative_Click;
+            btnPercentage.Click += BtnPercentage_Click;
+            btnEqual.Click += BtnEqual_Click;
+        }
+
+        private void BtnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnPercentage_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(lblResult.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                lblResult.Content = lastNumber.ToString();
+            }
+        }
+
+        private void BtnNegative_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(lblResult.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                lblResult.Content = lastNumber.ToString();
+            }
+        }
+
+        private void BtnAC_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = "0";
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(lblResult.Content.ToString(), out lastNumber))
+            {
+                lblResult.Content = "0";
+            }
+
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender == btnZero)
+                selectedValue = 0;
+            if (sender == btnOne)
+                selectedValue = 1;
+            if (sender == btnTwo)
+                selectedValue = 2;
+            if (sender == btnThree)
+                selectedValue = 3;
+            if (sender == btnFour)
+                selectedValue = 4;
+            if (sender == btnFive)
+                selectedValue = 5;
+            if (sender == btnSix)
+                selectedValue = 6;
+            if (sender == btnSeven)
+                selectedValue = 7;
+            if (sender == btnEight)
+                selectedValue = 8;
+            if (sender == btnNine)
+                selectedValue = 9;
+
+            if (lblResult.Content.ToString() == "0")
+            {
+                lblResult.Content = $"{selectedValue}";
+            }
+            else
+            {
+                lblResult.Content = $"{lblResult.Content}{selectedValue}";
+            }
         }
     }
 }
