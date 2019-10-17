cb3a9cf68debd7e7968ad4cc0f24b56b4b618d59
ejrach <eric.rach@gmail.com>
2019-10-17 13:39:02 -0500

    Defined Contact class

---
+++ DesktopContactsApp/Classes/Contact.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopContactsApp.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
