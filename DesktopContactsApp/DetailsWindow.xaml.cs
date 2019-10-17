4040b13233859017ae636e9382cdd9240df3fc72
ejrach <eric.rach@gmail.com>
2019-10-17 17:37:47 -0500

    Implemented Update and delete functionality to database

---
+++ DesktopContactsApp/DetailsWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DesktopContactsApp.Classes;
+using SQLite;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for DetailsWindow.xaml
+    /// </summary>
+    public partial class DetailsWindow : Window
+    {
+        Contact contact;
+
+        public DetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            this.contact = contact;
+
+            nameTextBox.Text = contact.Name;
+            phoneNumberTextBox.Text = contact.Phone;
+            emailTextBox.Text = contact.Email;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Phone = phoneNumberTextBox.Text;
+            contact.Email = emailTextBox.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();  //this is ignored if the table already exists
+                connection.Update(contact);
+            }
+
+            //Close the window
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();  //this is ignored if the table already exists
+                connection.Delete(contact);
+            }
+
+            //Close the window
+            Close();
+        }
+    }
+}
--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -21,9 +21,13 @@ namespace DesktopContactsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            contacts = new List<Contact>();
 
             //Read on initialze
             ReadDatabase();
@@ -40,17 +44,53 @@ namespace DesktopContactsApp
 
         void ReadDatabase()
         {
-            List<Contact> contacts;
+            
             
             using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
             {
-                connection.CreateTable<Contact>();          //generic method allows us to create a table if it doesn't exist. No error thrown.
-                contacts = connection.Table<Contact>().ToList(); //generic method allows us to read a table
+                connection.CreateTable<Contact>();                  //generic method allows us to create a table if it doesn't exist. No error thrown.
+                contacts = connection.Table<Contact>().ToList().OrderBy(c => c.Name).ToList();    //generic method allows us to read a table
+
+                //LINQ equivalent of above, example
+                //var variable = from c2 in contacts
+                //               orderby c2.Name
+                //               select c2;
             }
 
             if (contacts != null)
             {
                 contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            //What this is saying:
+            //  We want to take the contacts list and filter the contacts where the name contains whatever the user has written 
+            //  in the text box.
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            //LINQ query example
+            //var filteredList2 = (from c2 in contacts
+            //                    where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+            //                    orderby c2.Name
+            //                    select c2).ToList();
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+            if(selectedContact != null)
+            {
+                DetailsWindow detailsWindow = new DetailsWindow(selectedContact);
+                detailsWindow.ShowDialog();
+
+                ReadDatabase();
             }
         }
     }
