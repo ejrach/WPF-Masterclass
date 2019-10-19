c6ff5a0a16ffc5ccfbcd6ec225b8e7b6fa0ff972
ejrach <eric.rach@gmail.com>
2019-10-19 14:55:50 -0500

    Defined the Model

---
+++ WeatherApp/Model/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Model
+{
+    //These defintions were defined with the help of copying and pasting a JSON response from Accuweather into
+    //JSON utils (from the internet: https://jsonutils.com/) and then slightly modifiying.
+
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+}
---
+++ WeatherApp/Model/CurrentConditions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Model
+{
+    //These defintions were defined with the help of copying and pasting a JSON response from Accuweather into
+    //JSON utils (from the internet: https://jsonutils.com/) and then slightly modifiying.
+    public class Units
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Units Metric { get; set; }
+        public Units Imperial { get; set; }
+    }
+
+    public class CurrentConditions
+    {
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public object PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+}
