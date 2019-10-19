af0e775600895214d07a6f188ab446200cbe163e
ejrach <eric.rach@gmail.com>
2019-10-19 12:22:46 -0500

    Created a custom contact control

---
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DesktopContactsApp.Classes;
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        private Contact _contact;
+
+        public Contact Contact
+        {
+            get { return _contact; }
+            set 
+            { 
+                _contact = value;
+                nameTextBlock.Text = _contact.Name;
+                phoneTextBlock.Text = _contact.Phone;
+                emailTextBlock.Text = _contact.Email;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
