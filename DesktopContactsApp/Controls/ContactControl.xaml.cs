da586cc41bde0b1eb00f89f02f6cf022ea53ee24
ejrach <eric.rach@gmail.com>
2019-10-19 12:56:07 -0500

    Binding the custom contact control in XAML to data using dependency property

--- DesktopContactsApp/Controls/ContactControl.xaml.cs
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -19,17 +19,34 @@ namespace DesktopContactsApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact _contact;
-
+        //Use the "propdp" snippet to create a dependency property. This allows us to use a control in XAML
         public Contact Contact
         {
-            get { return _contact; }
-            set 
-            { 
-                _contact = value;
-                nameTextBlock.Text = _contact.Name;
-                phoneTextBlock.Text = _contact.Phone;
-                emailTextBlock.Text = _contact.Email;
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", 
+                typeof(Contact), 
+                typeof(ContactControl), 
+                new PropertyMetadata(new Contact() { 
+                    Name ="Name Lastname", 
+                    Phone = "123-456-7890", 
+                    Email = "email@domain.com" }
+                ,SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+
+            if (control != null)
+            {
+                //We get the NewValue that is assigned to the contact
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
             }
         }
 
