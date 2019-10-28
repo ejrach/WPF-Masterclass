e9c9540177b6ea4c98ddf71bde276cf523387fb7
ejrach <eric.rach@gmail.com>
2019-10-28 11:50:09 -0500

    Initial commit

---
+++ NotesApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesApp.Models
+{
+    public class User
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+}
